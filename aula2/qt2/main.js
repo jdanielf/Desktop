a4bdb217d427fc4c030668b14ea2fc55d2b9acb6
Daniel Ferreira <danielljd@hotmail.com>
2025-10-27 17:00:50 -0300

    qt2

--- aula2/qt2/main.js
+++ aula2/qt2/main.js
@@ -3,11 +3,11 @@ const criaJanela = () =>{
     const janela = new BrowserWindow({
          width: 1800 , 
          height: 1200,
-         title: "Conversor de Moedas",
-         resizable: false,
-         fullscree: false,
-         backgroundColor:"blue" ,
-         icon:'com.png'
+        //  title: "Conversor de Moedas",
+        //  resizable: false,
+        //  fullscree: false,
+        //  backgroundColor:"blue" ,
+        //  icon:'com.png'
 
         })
 janela.loadFile('qt1.html')
