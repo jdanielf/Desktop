32419b6199d8994ab744f6e4b3dd35bc2501fc36
Daniel Ferreira <danielljd@hotmail.com>
2025-10-30 14:49:23 -0300

    Salvando alterações locais antes do pull

--- aula2/qt2/funcao.js
+++ aula2/qt2/funcao.js
@@ -1,18 +1,33 @@
-function    mediaAnual(){
-    let n1 = Number(document.getElementById("n1").value)
-    let  n2 = Number(document.getElementById("n2").value)
-    let  n3 = Number(document.getElementById("n3").value)
-    let  n4 = Number(document.getElementById("n4").value)
+function calcularMedia() {
+    
+            let nota1 = Number(document.getElementById("nota1").value)
+            let nota2 = Number(document.getElementById("nota2").value)
+            let nota3 = Number(document.getElementById("nota3").value)
+            let nota4 = Number(document.getElementById("nota4").value)
 
-    let media = (n1 + n2 + n3 + n4 ) /4
+             if (
+        isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4) ) 
+        {alert("Preencha todas as notas!")
+        return
+        }
 
-    document.getElementById("resultado").innerHTML = `A média anual é:  ${media.toFixed(2)}`
-}
+            let media = (nota1 + nota2 + nota3 + nota4) / 4
 
-function limpar(){
-    document.getElementById("n1").value = ""
-    document.getElementById("n2").value = ""
-    document.getElementById("n3").value = ""
-    document.getElementById("n4").value = ""
-    document.getElementById("resultado").innerHTML = ""
-}
+            if(media >= 7) {
+                document.getElementById("resultado").innerText = "Parabéns! Você foi aprovado com média: " + media.toFixed(2)
+            }else if(media >= 4.9 && media < 7) {
+                document.getElementById("resultado").innerText = "Você está de recuperação com média: " + media.toFixed(2)
+            }else if(media < 4) {
+                document.getElementById("resultado").innerText = "Infelizmente você foi reprovado com média: " + media.toFixed(2)
+            }
+            alert("Sua média é: " + media.toFixed(2))
+            document.getElementById("resultado").innerText = "Sua média anual é: " + media.toFixed(2)
+        }
+        function limparCampos() {
+   document.getElementById("nota1").value = ""
+  document.getElementById("nota2").value = ""
+  document.getElementById("nota3").value = ""
+  document.getElementById("nota4").value = ""
+  document.getElementById("resultado").innerText = ""
+  fecharAlerta()
+        }   
