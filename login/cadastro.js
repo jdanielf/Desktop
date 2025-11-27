9a2415ce2fc4b3353065eae253c327b571704e28
jdanielf <danielljd@hotmail.com>
2025-11-27 16:26:18 -0300

    menu bloco de notas

--- login/cadastro.js
+++ login/cadastro.js
@@ -7,7 +7,31 @@ document.getElementById('cadastrar').addEventListener('click', () => {
     let numero = document.getElementById("numero").value
     let senha = document.getElementById("senha").value
    
-    
+    if(nome === '' || cpf === '' || telefone === '' || bairro === '' || rua === '' || numero === '' || senha === ''){
+        alert('Preencha todos os campos')
+        return
+    }if(cpf.length != 11){
+        alert('CPF deve conter 11 digitos')
+        return
+    }   if(telefone.length < 10){
+        alert('Telefone deve conter no minimo 10 digitos')
+        return
+    }      if(senha.length < 6){
+                alert('Senha deve conter no minimo 6 digitos')
+        return
+    }  if(isNaN(numero)){
+        alert('Numero deve ser um valor numerico')
+        return
+    }      if(isNaN(telefone)){
+        alert('Telefone deve ser um valor numerico')
+        return
+    }      if(isNaN(cpf)){
+        alert('CPF deve ser um valor numerico')
+        return
+    }   if(numero <= 0){
+        alert('Numero deve ser maior que zero')
+        return
+    }   
 //    window.api.cadastro({nome,senha,cpf,telefone,bairro,rua,numero})// enviando os adados para o main.js
    //recebendo a resposta do main.js 
    window.api.cadastro({nome,cpf,telefone,bairro,rua,numero,senha}).then((result) => {
